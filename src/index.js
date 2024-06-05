65a5955b6360cfdaeceba183ecbdfa13d320f6ba
shinevue <shine401020@gmail.com>
2024-06-06 05:59:15 +1000

    Refactor index.js to call functions by their groups of features

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-const { isFileExists, getChromeProfilePath, getChromeExecutablePath, loadNotBlankPage } = require('./helpers');
-const { scrapeWithPagination, autoScroll, createFile, saveProduct, navigatePage } = require('./scraper');
-const { clusterWrapper } = require('./wrapper');
-
-module.exports = {
+const {
 	isFileExists,
+	createFile,
+	getWebName,
+	url2FileName,
 	getChromeProfilePath,
 	getChromeExecutablePath,
 	loadNotBlankPage,
-	scrapeWithPagination,
-	createFile,
-	autoScroll,
-	saveProduct,
-	navigatePage,
+} = require('./helpers');
+const { scrapeWithPagination, autoScroll, saveProduct, navigatePage } = require('./scraper');
+const { clusterWrapper } = require('./wrapper');
+
+module.exports = {
+	helpers: {
+		isFileExists,
+		createFile,
+		getWebName,
+		url2FileName,
+		getChromeProfilePath,
+		getChromeExecutablePath,
+		loadNotBlankPage,
+	},
+	scraper: { scrapeWithPagination, autoScroll, saveProduct, navigatePage },
 	clusterWrapper,
 };
